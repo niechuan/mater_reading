476fefa2981062851644d18235102ab0f25ed08f
nie.c <3127510638@qq.com>
2019-08-15 10:34:42 +0800

    mater_reading

---
+++ src/main/java/com/yskj/mater/MaterApplication.java
@@ -0,0 +1,13 @@
+package com.yskj.mater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterApplication.class, args);
+	}
+
+}
