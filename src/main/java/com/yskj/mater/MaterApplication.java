4603e5187c083f1a3b708d8021ed5f3924a055d6
nie.c <3127510638@qq.com>
2019-08-15 14:11:23 +0800

    通过meaven添加Sqlserver 连接驱动，springboot_mybatis,mybatis_generator 基础配置

--- src/main/java/com/yskj/mater/MaterApplication.java
+++ src/main/java/com/yskj/mater/MaterApplication.java
@@ -1,9 +1,13 @@
 package com.yskj.mater;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan(value = "com.yskj.mater.*.*.dao")
 public class MaterApplication {
 
 	public static void main(String[] args) {
